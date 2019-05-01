34c7fbad1facb8f4f4a4e2ff0cc71e08eda7a173
Abdul <awsayed@alaska.edu>
2019-05-01 02:32:23 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+def my_collect(array)
+	collect = []
+  i = 0
+	while i < array.length
+		collect.push( yield(array[i]) )
+		i += 1
+	end
+	return collect
+end
 
 
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+
+my_collect(languages) { |lang|
+	lang.upcase
+}
+
+my_collect(students) { |student|
+	student.split(" ").first
+}
